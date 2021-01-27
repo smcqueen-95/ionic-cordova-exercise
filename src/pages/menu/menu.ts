51c165c06261f7ae67bca232f61732e3ff84ccc7
Samira Mc Queen <samira.mcqueen11@gmail.com>
2021-01-27 09:22:01 -0400

    Alerts, Toast, Loading

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 import { Dish } from '../../shared/dish';
 import { DishProvider } from '../../providers/dish/dish';
 import { DishdetailPage } from '../dishdetail/dishdetail';
@@ -24,6 +24,7 @@ export class MenuPage implements OnInit {
   constructor(public navCtrl: NavController, public navParams: NavParams,
     private dishService: DishProvider,
     private favoriteservice: FavoriteProvider,
+    private toastCtrl: ToastController,
     @Inject ('BaseURL') private BaseURL) {
   }
 
@@ -40,6 +41,9 @@ export class MenuPage implements OnInit {
   addToFavorites(dish: Dish) {
     console.log('Adding to Favorites', dish.id);
     this.favoriteservice.addFavorite(dish.id);
+    this.toastCtrl.create({
+      message: 'Dish ' + dish.id + ' added as favorite successfully',
+      duration: 3000}).present();
   }
   
   dishSelected(event, dish) {
