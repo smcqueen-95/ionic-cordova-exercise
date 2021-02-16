35eb9715287c0126ae19df2c00f047e37a00c4a1
Samira Mc Queen <samira.mcqueen11@gmail.com>
2021-02-16 16:40:46 -0400

    Storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import {HttpModule} from '@angular/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { ErrorHandler, NgModule } from "@angular/core";
+import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
+import { HttpModule } from "@angular/http";
+import { IonicStorageModule } from "@ionic/storage";
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { AboutPage } from '../pages/about/about';
-import { MenuPage } from '../pages/menu/menu';
-import { ContactPage } from '../pages/contact/contact';
-import {DishdetailPage} from '../pages/dishdetail/dishdetail';
-import {FavoritesPage} from '../pages/favorites/favorites';
-import {ReservationPage} from '../pages/reservation/reservation';
-import {CommentsPage} from '../pages/comments/comments';
+import { MyApp } from "./app.component";
+import { HomePage } from "../pages/home/home";
+import { AboutPage } from "../pages/about/about";
+import { MenuPage } from "../pages/menu/menu";
+import { ContactPage } from "../pages/contact/contact";
+import { DishdetailPage } from "../pages/dishdetail/dishdetail";
+import { FavoritesPage } from "../pages/favorites/favorites";
+import { ReservationPage } from "../pages/reservation/reservation";
+import { CommentsPage } from "../pages/comments/comments";
+import { LoginPage } from "../pages/login/login";
 
-import {baseURL} from '../shared/baseurl';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { DishProvider } from '../providers/dish/dish';
-import { LeaderProvider } from '../providers/leader/leader';
-import { PromotionProvider } from '../providers/promotion/promotion';
-import { ProcessHttpmsgProvider } from '../providers/process-httpmsg/process-httpmsg';
-import { FavoriteProvider } from '../providers/favorite/favorite';
+import { baseURL } from "../shared/baseurl";
+import { StatusBar } from "@ionic-native/status-bar";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { DishProvider } from "../providers/dish/dish";
+import { LeaderProvider } from "../providers/leader/leader";
+import { PromotionProvider } from "../providers/promotion/promotion";
+import { ProcessHttpmsgProvider } from "../providers/process-httpmsg/process-httpmsg";
+import { FavoriteProvider } from "../providers/favorite/favorite";
 
 @NgModule({
   declarations: [
@@ -33,11 +35,13 @@ import { FavoriteProvider } from '../providers/favorite/favorite';
     FavoritesPage,
     ReservationPage,
     CommentsPage,
+    LoginPage,
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
     HttpModule,
+    IonicStorageModule.forRoot(),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -50,17 +54,18 @@ import { FavoriteProvider } from '../providers/favorite/favorite';
     FavoritesPage,
     ReservationPage,
     CommentsPage,
+    LoginPage,
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
     DishProvider,
     LeaderProvider,
     PromotionProvider,
     ProcessHttpmsgProvider,
-    {provide: 'BaseURL', useValue: baseURL},
+    { provide: "BaseURL", useValue: baseURL },
     FavoriteProvider,
-  ]
+  ],
 })
 export class AppModule {}
